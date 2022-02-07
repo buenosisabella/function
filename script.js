4fad6d3ff61fbef4bac281298ddbaa89791ae82b
buenosisabella <coreamilisabella@gmail.com>
2022-02-07 00:37:32 -0300

    realizado com sucesso

---
+++ script.js
@@ -0,0 +1,36 @@
+function mensagem(){
+    console.log(" oi estou presente aqui ")
+}
+
+mensagem()
+
+function mostrarNome(nome){
+    console.log(`eu me chamo ${nome}`)
+}
+
+mostrarNome(`Isabella Bueno`)
+
+function vida(nome, musica, idade){
+    return nome, musica, idade
+}
+    let nome = "Isabella"
+    let musica = "Golpe Internacional"
+    let idade = 17
+
+    console.log(`me chamo ${nome} o meu estilo musica e ${musica} e tenho ${idade} anos`);
+
+
+function diverção( filme, Musica){
+    return filme, musica
+}
+ let filme = "it a coisa"
+ let Musica = "Oceans"
+
+ console.log(`meu filme favorito e ${filme} e a minha musica ${Musica}`)
+
+
+ function triplo (x){
+     return console.log(x*3)
+ }
+
+ triplo(4)
